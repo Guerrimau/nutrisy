345888bd2e07eddbbdfd69faa2664de9314a8bda
Gerardo Mayboca <gmayboca@gmail.com>
2021-05-25 17:10:25 -0700

    Se hizo una tabla correctamente

--- src/pages/Binnacles.js
+++ src/pages/Binnacles.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Divider } from "@material-ui/core";
+import { TableContainer, Divider } from "@material-ui/core";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import { ipcRenderer as ipc } from 'electron';
 import "../styles/table.css";
 
@@ -19,34 +25,49 @@ export const Binnacles = () => {
         <div>
             <h1>Bitácoras</h1>
             <Divider />
-
             <h3>Comidas Insertadas</h3>
-            <table className="border">
-                <tr>
-                    <th>id</th>
-                    <th>comidaId</th>
-                    <th>host</th>
-                    <th>usuario</th>
-                    <th>accion</th>
-                    <th>fecha</th>
-                </tr>
-                {
-                    comidasInsertadas.map(item => {
-                        const fecha = new Date(item.fecha).toISOString()
-
-                        return (
-                            <tr key="item.id">
-                                <td>{item.id}</td>
-                                <td>{item.comidaId}</td>
-                                <td>{item.host}</td>
-                                <td>{item.usuario}</td>
-                                <td>{item.accion}</td>
-                                <td>{fecha}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </table>
+            <TableContainer style={{ overflow: "hidden"}} component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>id</TableCell>
+                            <TableCell>comidaId</TableCell>
+                            <TableCell>host</TableCell>
+                            <TableCell>usuario</TableCell>
+                            <TableCell>accion</TableCell>
+                            <TableCell>fecha</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {comidasInsertadas !== undefined ?
+                            comidasInsertadas.map((item) => {
+                                const fecha = new Date(item.fecha).toISOString()
+                                return (
+                                    <TableRow key={item.id}>
+                                    <TableCell component="th" scope="row" width='2.5em'>
+                                        {item.id}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" width='2.5em'>
+                                        {item.comidaId}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" width='2.5em'>
+                                        {item.host}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" width='2.5em'>
+                                        {item.usuario}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" width='2.5em'>
+                                        {item.accion}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" width='2.5em'>
+                                        {fecha}
+                                    </TableCell>
+                                </TableRow>
+                                );
+                            }) : null}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
