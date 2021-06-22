fa7261faadc5e54684b85b6e1005cca99de379e0
Gerardo Mayboca <gmayboca@gmail.com>
2021-06-21 17:51:48 -0700

    Se actualizaron los componentes del formulario

--- src/components/diet/NewDietDialog.js
+++ src/components/diet/NewDietDialog.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useForm } from "../../hooks/useForm";
-import { MenuItem } from "@material-ui/core";
+import { MenuItem, Select, InputLabel } from "@material-ui/core";
+import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers"
+import DateFnsUtils from "@date-io/date-fns";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -14,11 +16,19 @@ export function NewDietDialog({ open, onClose }) {
 
     const { crearDieta, pacientes = [], traerPacientes } = useContext(DietasContext);
 
-    const [formValues, handleInputChange] = useForm({
+    const [formValues, handleInputChange, setValues] = useForm({
         pacienteId: '',
         nombreDieta: '',
-        fechaInicio: '',
+        fechaInicio: new Date().toISOString(),
     });
+
+    const handleDateChange = (e) => {
+        const date = new Date(e).toISOString()
+        setValues(prevState => ({
+            ...formValues,
+            fechaInicio: date
+        }))
+    }
 
     const handlePostDiet = () => {
         crearDieta(formValues);
@@ -30,52 +40,51 @@ export function NewDietDialog({ open, onClose }) {
     }, [])
 
     return (
-        <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Nueva dieta</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="pacienteId"
-                    name="pacienteId"
-                    label="Paciente"
-                    type="text"
-                    fullWidth
-                    select
-                    onChange={handleInputChange}>
-                    {
-                        pacientes.map(paciente => (
-                            <MenuItem key={paciente.pacienteId} value={paciente.pacienteId}>{paciente.nombreCompleto}</MenuItem>
-                        ))
-                    }
-                </TextField>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="nombreDieta"
-                    label="Nombre de Dieta"
-                    type="text"
-                    fullWidth
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="fechaInicio"
-                    label="Fecha de Inicio"
-                    type="date"
-                    fullWidth
-                    onChange={handleInputChange}
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose} color="primary">
-                    Cancelar
-          </Button>
-                <Button onClick={handlePostDiet} color="primary">
-                    Agregar
-          </Button>
-            </DialogActions>
-        </Dialog>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Nueva dieta</DialogTitle>
+                <DialogContent>
+                    <InputLabel id="pacienteLbl">Paciente</InputLabel>
+                    <Select
+                        id="pacienteId"
+                        name="pacienteId"
+                        labelId="pacienteLbl"
+                        label="Paciente"
+                        style={{ minWidth: "551px" }}
+                        onChange={handleInputChange}>
+                        {
+                            pacientes.map(paciente => (
+                                <MenuItem key={paciente.pacienteId} value={paciente.pacienteId}>{paciente.nombreCompleto}</MenuItem>
+                            ))
+                        }
+                    </ Select>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="nombreDieta"
+                        label="Nombre de Dieta"
+                        type="text"
+                        fullWidth
+                        onChange={handleInputChange}
+                    />
+                    <DatePicker
+                        format="dd/MM/yyyy"
+                        id="fechaInicio"
+                        label="Fecha de inicio"
+                        style={{ minWidth: "551px" }}
+                        value={formValues.fechaInicio}
+                        onChange={handleDateChange}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handlePostDiet} color="primary">
+                        Agregar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </MuiPickersUtilsProvider>
     );
 }
